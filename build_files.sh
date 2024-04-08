a452d0c5cdf5c4791e9fdfd2eb4990edfc679e1f
jagadish <jagadish.nadar41@gmail.com>
2024-04-08 22:49:25 +0530

    build file up

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 echo " BUILD START"
+python3.9 -m ensurepip --upgrade  # Ensure pip is installed
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
-echo " BUILD END" 
+echo " BUILD END"
